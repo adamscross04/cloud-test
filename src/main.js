d55c7f80d5be55f52456f4112d4d64cf70fbe8d6
Adam Cross <adam.cross@anana.com>
2023-06-09 13:53:49 +0100

    for the jp

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+    let urlParams = new URLSearchParams(window.location.search);
+    alert(`Your interaction id is: ${urlParams.get('id')}`);
